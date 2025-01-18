996d7935f63d71358a9b0f2795792fbef7ac6b85
s.elyounsi <s.elyounsi@wwwe.de>
2025-01-18 18:38:14 +0100

    feat: merge WPStats and OrderStats into unified API endpoint

--- data/order_stats/routes.php
+++ data/order_stats/routes.php
@@ -1,11 +1,25 @@
 <?php
 
+use Utils\Stats\OrderStats;
+use Utils\Stats\WPStats;
+
 add_action('rest_api_init', function () {
     register_rest_route('wc/v3', '/orders/stats', [
         'methods'  => 'GET',
-        'callback' => [new \Utils\Stats\OrderStats(), 'getOrderStats'],
+        'callback' => 'get_orders_count',
         'permission_callback' => function () {
             return current_user_can('manage_woocommerce');
         }
     ]);
 });
+
+function get_orders_count() 
+{
+    $wp_stats = new WPStats(); 
+    $orderStats = new OrderStats();
+
+    return new WP_REST_Response([
+        "orders" => $orderStats->getOrderStats() ?? null,
+        "visitors" => $wp_stats->getStats() ?? null 
+    ], 200);
+}
