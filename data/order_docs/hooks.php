4f443e62ee16fbc52d79e7d8812b10fa9852bd3d
selyounsi <sami.el-younsi@hotmail.com>
2025-10-21 20:05:48 +0200

    fix: prevent fatal error when  is null in custom_wcpdf_template_path

--- data/order_docs/hooks.php
+++ data/order_docs/hooks.php
@@ -13,17 +13,21 @@ Positions::setPositions();
 add_filter('wpo_wcpdf_template_file', 'custom_wcpdf_template_path', 10, 3);
 function custom_wcpdf_template_path($template_path, $document_type, $order) 
 {
-    if($order->get_meta('invoice_data')) {     
+    if (!is_object($order) || !method_exists($order, 'get_meta')) {
+        return $template_path; 
+    }
+
+    if ($order->get_meta('invoice_data')) {     
         $custom_template_path = WHA_PLUGIN_PATH . '/data/order_docs/templates/wha-invoice.php';
-    } else if ($document_type === 'invoice') {
+    } elseif ($document_type === 'invoice') {
         $custom_template_path = WHA_PLUGIN_PATH . '/data/order_docs/templates/woo-invoice.php';  
     }
 
     if (isset($custom_template_path) && file_exists($custom_template_path)) {
         return $custom_template_path;
-    } else {
-        return $template_path;
     }
+
+    return $template_path;
 }
 
 /**
